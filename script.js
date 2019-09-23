ecfd3ba8c7e90aaa3d377d44a62a5ac2309197b0
Art Tenorio <aten@udel.edu>
2019-09-23 05:35:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,16 +6,31 @@ $(document).ready(function(){
          $("#bingos").append(`<li>${rack.rack}: 
          <span class="answer hidden">${rack.words.split("@@")}</span></li>`);
       });
+    
       $("#bingos li").on("click", function(evt){
         $(evt.currentTarget).find(".answer").toggleClass("hidden");
       });
     }
+    
+       //this checks to see if the user input is a possible word
+     // if so, give them a point, if not do nothing but let them know its wrong
+     let check = function(submission){
+        return false;
+    }
+    
+    
     //user submits their answer and checks against the possible words...
     $("#go").on("click", function(){
         var submission = document.getElementById("theanswer").value;
-        // if user is correct let them know... else...
-        alert(submission);
+        if(check(submission)) {
+            alert("You Are Correct!");
+
+        } else {
+            alert("Wrong Answer, Try Again!");
+        }
       });
+     
+  
       
     //get the twist!
     $("#grabmore").on("click", function(){
