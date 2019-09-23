09c3a541022ad3437d0a7e43740fc3ac5ffacfa0
Art Tenorio <aten@udel.edu>
2019-09-23 06:10:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
       
+
     let showRacks = function(racks){
+
       $("#bingos").html('');
       racks.map(rack=>{
          $("#bingos").append(`<li>${rack.rack}: 
-         <span class="answer hidden">${rack.words.split("@@")}</span></li>`);
+         <span class="answer hidden" id="therackwords">${rack.words.split("@@")}</span></li>`);
       });
     
       $("#bingos li").on("click", function(evt){
@@ -15,13 +17,15 @@ $(document).ready(function(){
        //this checks to see if the user input is a possible word
      // if so, give them a point, if not do nothing but let them know its wrong
      let check = function(submission){
-        return false;
+         alert(document.getElementById("therackwords").innerHTML);
+        alert(shit);
     }
     
     
     //user submits their answer and checks against the possible words...
     $("#go").on("click", function(){
         var submission = document.getElementById("theanswer").value;
+        //alert(submission);
         if(check(submission)) {
             alert("You Are Correct!");
 
